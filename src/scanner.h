27f9d2ac7a8934b8f340fd7972a798dfdfca2074
Pipao1229 <felibenavides12@gmail.com>
2026-08-17 19:47:44 -0600

    Add scanner token interface

---
+++ src/scanner.h
@@ -0,0 +1,27 @@
+#ifndef SCANNER_H
+#define SCANNER_H
+
+#define MAX_IDENTIFIER_LENGTH 32
+
+typedef enum token_types {
+    BEGIN,
+    END,
+    READ,
+    WRITE,
+    ID,
+    INTLITERAL,
+    LPAREN,
+    RPAREN,
+    SEMICOLON,
+    COMMA,
+    ASSIGNOP,
+    PLUSOP,
+    MINUSOP,
+    SCANEOF
+} token;
+
+extern char token_buffer[];
+
+token scanner(void);
+
+#endif
